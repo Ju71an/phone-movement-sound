41fdaadcc93c46638e8846931a56f8560f6a8737
Ju71an <131834638+Ju71an@users.noreply.github.com>
2023-04-25 21:55:40 -0500

    Add files via upload

---
+++ phone-movement-sound-a61b18a14e9bd63fec665bf6b67f65cbd303aa46/phone-movement-sound-a61b18a14e9bd63fec665bf6b67f65cbd303aa46/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Julian-Rios/pen/RweVoKp](https://codepen.io/Julian-Rios/pen/RweVoKp).
+
---
+++ phone-movement-sound-a61b18a14e9bd63fec665bf6b67f65cbd303aa46/phone-movement-sound-a61b18a14e9bd63fec665bf6b67f65cbd303aa46/src/script.ts
@@ -0,0 +1,36 @@
+// Crear un contexto de audio
+const audioContext = new Tone.Context();
+
+// Crear un oscilador
+const oscillator = new Tone.Oscillator().toDestination();
+
+// Crear un objeto que capture los datos del acelerómetro
+const accelerometer = new Tone.Motion().start();
+
+// Conectar los datos del acelerómetro al oscilador para cambiar la frecuencia y el volumen
+accelerometer.connect((value) => {
+  oscillator.frequency.value = value.x * 20;
+  oscillator.volume.value = value.y * 2;
+});
+
+// Iniciar el oscilador
+oscillator.start();
+
+// Dibujar la representación visual del movimiento del teléfono
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
+ctx.translate(width / 2, height / 2);
+function draw(value) {
+  ctx.clearRect(-width / 2, -height / 2, width, height);
+  const x = value.x * (width / 2);
+  const y = value.y * (height / 2);
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fill();
+  requestAnimationFrame(() => {
+    draw(accelerometer.value);
+  });
+}
+draw(accelerometer.value);
